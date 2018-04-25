8ce14fe00164c30a8290c3c5868665d95b78b455
Leonardo <leonardofaria.dev@gmail.com>
2018-04-25 12:59:17 -0300

    Inicio main mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,11 @@ if (screen.width < 991) {
 	// Itens menu
 	$('.nav-item').addClass('col-md-12');
 	$('.lista-menu').addClass('text-center');
+	$('.titulo').addClass('d-none');
+	$('.subtitulo').addClass('d-none');
+	$('.item-mobile').removeClass('col-md-12');
+	$('.item-mobile').addClass('col-sm-4');
+	$('.item-mobile').removeClass('d-none');
+
 }	
+
